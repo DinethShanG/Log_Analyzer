0ccabe95f428be6de6c7910bf9dafb9fffb12b07
randiayeshanikossinna <randiayeshani@gmail.com>
2020-09-15 10:35:01 -0700

    Added DataBase Connection

--- src/com/ConstructionTeam/DBConnector.java
+++ src/com/ConstructionTeam/DBConnector.java
@@ -1,4 +1,39 @@
 package com.ConstructionTeam;
 
+import java.sql.*;
+
+
 public class DBConnector {
+    public Connection connection;
+    private Statement statement;
+    public ResultSet resultSet;
+
+    //create constructor
+    public DBConnector() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/constructionteamdb", "root", "");
+            statement = connection.createStatement();
+
+        } catch (Exception ex) {
+            System.out.println();
+        }
+    }
+
+    public void getData() {
+        try {
+
+            String query = "select * from email";
+            resultSet = statement.executeQuery(query);
+            System.out.println("Email from Database");
+            while (resultSet.next()) {
+                String result = resultSet.getString("email");
+                System.out.println(result);
+
+            }
+        } catch (Exception ex) {
+            System.out.println();
+        }
+
+    }
 }
