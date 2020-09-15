60b4b228940bf60b0f2bc8564da4fa939a9f8c83
DinethShanG <gimhana.ds@gmail.com>
2020-09-15 18:48:42 +0530

    Cleared Main

--- src/main/java/com/ConstructionTeam/Main.java
+++ src/main/java/com/ConstructionTeam/Main.java
@@ -3,6 +3,7 @@ package com.ConstructionTeam;
 public class Main {
 
     public static void main(String[] args) {
-        
+         /*LogFileReader logFileReader = new LogFileReader();
+        logFileReader.logFileReader();*/
     }
 }
