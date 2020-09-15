5a0b3619025f9b68617aa2cef8073be6195a894b
thilina <thilinam4@gmail.com>
2020-09-15 16:58:02 +0530

    initializing-v4 the Log file reader -thilina

--- src/com/ConstructionTeam/ErrorResult.java
+++ src/com/ConstructionTeam/ErrorResult.java
@@ -40,34 +40,4 @@ public class ErrorResult {
     public void setErrorDescription(String errorDescription) {
         this.errorDescription = errorDescription;
     }
-
-
-
-    public static void main(String[] args) throws ParseException {
-        String fileTimeStamp;
-
-
-        LastAccessFileReader lastAccessFileReader=new LastAccessFileReader();
-        fileTimeStamp=lastAccessFileReader.FileReader();
-
-        LogFileReader logFileReader=new LogFileReader();
-        List<ErrorResult> results =logFileReader.logFileReader("/media/shaggy/New Volume/project/Java/construction_final/Log_Analyzer/idea.log",fileTimeStamp);
-
-
-        CurrentDateAndTime currentDateAndTime=new CurrentDateAndTime();
-        LastAccessFileWriter lastAccessFileWriter=new LastAccessFileWriter();
-        lastAccessFileWriter.writeFile(currentDateAndTime.getCurrentDateAndTime());
-
-
-
-
-
-
-    }
-
-
-
-
-
-
 }
