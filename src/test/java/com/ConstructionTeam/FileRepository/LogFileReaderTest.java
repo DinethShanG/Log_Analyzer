a250f7baf1c832d0750d12c7729fb1f498500c34
Mangalan <mangalan.se.1998@gmail.com>
2020-09-17 22:14:00 +0530

    Create test class for LogFileReader, But face some problems i.e datatype ErrorData

---
+++ src/test/java/com/ConstructionTeam/FileRepository/LogFileReaderTest.java
@@ -0,0 +1,19 @@
+package com.ConstructionTeam.FileRepository;
+
+import static org.junit.Assert.*;
+
+import com.ConstructionTeam.DataModels.ErrorData;
+import java.util.ArrayList;
+import org.junit.Test;
+
+public class LogFileReaderTest {
+
+  @Test
+  public void should_return_error_data_list(){
+    LogFileReader logFileReader = new LogFileReader();
+    ArrayList<ErrorData> actualErrorDataList = logFileReader.getData("src/test/java/com/ConstructionTeam/FileRepository/LogFileReaderTest.log","2020-01-25 16:07:06");
+    ArrayList<ErrorData> expectedErrorDataList=new ArrayList<ErrorData>();
+    expectedErrorDataList.add();
+  }
+
+}
